fe803170fa3b0f17ef70b9c7d091856bdfa4a4ab
Ngetsophun <phunofficial18@gmail.com>
2022-12-05 22:36:08 +0700

    School Management

---
+++ School_Management/Students.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace School_Management
+{
+    partial class Students
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnclose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StName = new System.Windows.Forms.TextBox();
+            this.StGencb = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DOBPicker = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FeesTb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbclass = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.AddressTb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.StudentGV = new Guna.UI2.WinForms.Guna2DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.StudentGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnclose
+            // 
+            this.btnclose.BackColor = System.Drawing.Color.Red;
+            this.btnclose.Location = new System.Drawing.Point(1046, -6);
+            this.btnclose.Name = "btnclose";
+            this.btnclose.Size = new System.Drawing.Size(66, 43);
+            this.btnclose.TabIndex = 1;
+            this.btnclose.Text = "X";
+            this.btnclose.UseVisualStyleBackColor = false;
+            this.btnclose.Click += new System.EventHandler(this.btnclose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // StName
+            // 
+            this.StName.Location = new System.Drawing.Point(115, 67);
+            this.StName.Name = "StName";
+            this.StName.Size = new System.Drawing.Size(179, 20);
+            this.StName.TabIndex = 3;
+            // 
+            // StGencb
+            // 
+            this.StGencb.FormattingEnabled = true;
+            this.StGencb.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.StGencb.Location = new System.Drawing.Point(115, 120);
+            this.StGencb.Name = "StGencb";
+            this.StGencb.Size = new System.Drawing.Size(179, 21);
+            this.StGencb.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Gender";
+            // 
+            // DOBPicker
+            // 
+            this.DOBPicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DOBPicker.Location = new System.Drawing.Point(115, 189);
+            this.DOBPicker.Name = "DOBPicker";
+            this.DOBPicker.Size = new System.Drawing.Size(179, 20);
+            this.DOBPicker.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "DOB";
+            // 
+            // FeesTb
+            // 
+            this.FeesTb.Location = new System.Drawing.Point(115, 309);
+            this.FeesTb.Name = "FeesTb";
+            this.FeesTb.Size = new System.Drawing.Size(179, 20);
+            this.FeesTb.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(40, 309);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Fees";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Class";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // cbclass
+            // 
+            this.cbclass.FormattingEnabled = true;
+            this.cbclass.Items.AddRange(new object[] {
+            "11A",
+            "12A"});
+            this.cbclass.Location = new System.Drawing.Point(115, 258);
+            this.cbclass.Name = "cbclass";
+            this.cbclass.Size = new System.Drawing.Size(179, 21);
+            this.cbclass.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(639, 526);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 55);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 526);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 55);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnadd
+            // 
+            this.btnadd.Location = new System.Drawing.Point(260, 526);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(100, 55);
+            this.btnadd.TabIndex = 14;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(71, 526);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 55);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Cancel";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // AddressTb
+            // 
+            this.AddressTb.Location = new System.Drawing.Point(120, 363);
+            this.AddressTb.Name = "AddressTb";
+            this.AddressTb.Size = new System.Drawing.Size(179, 20);
+            this.AddressTb.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(45, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Address";
+            // 
+            // StudentGV
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.StudentGV.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.StudentGV.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.StudentGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.StudentGV.DefaultCellStyle = dataGridViewCellStyle3;
+            this.StudentGV.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.StudentGV.Location = new System.Drawing.Point(392, 87);
+            this.StudentGV.Name = "StudentGV";
+            this.StudentGV.RowHeadersVisible = false;
+            this.StudentGV.Size = new System.Drawing.Size(556, 332);
+            this.StudentGV.TabIndex = 19;
+            this.StudentGV.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.StudentGV.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.StudentGV.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.StudentGV.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.StudentGV.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.StudentGV.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.StudentGV.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.StudentGV.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.StudentGV.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.StudentGV.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentGV.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.StudentGV.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.StudentGV.ThemeStyle.HeaderStyle.Height = 4;
+            this.StudentGV.ThemeStyle.ReadOnly = false;
+            this.StudentGV.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.StudentGV.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.StudentGV.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StudentGV.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.StudentGV.ThemeStyle.RowsStyle.Height = 22;
+            this.StudentGV.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.StudentGV.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.StudentGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.StudentGV_CellContentClick_1);
+            // 
+            // Students
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1105, 708);
+            this.Controls.Add(this.StudentGV);
+            this.Controls.Add(this.AddressTb);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbclass);
+            this.Controls.Add(this.FeesTb);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DOBPicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.StGencb);
+            this.Controls.Add(this.StName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnclose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Students";
+            this.Text = "Students";
+            ((System.ComponentModel.ISupportInitialize)(this.StudentGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnclose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox StName;
+        private System.Windows.Forms.ComboBox StGencb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DOBPicker;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FeesTb;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbclass;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnadd;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox AddressTb;
+        private System.Windows.Forms.Label label6;
+        private Guna.UI2.WinForms.Guna2DataGridView StudentGV;
+    }
+}
---
+++ School_Management/Students.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace School_Management
+{
+    public partial class Students : Form
+    {
+        public Students()
+        {
+            InitializeComponent();
+            DisplayStudent();
+        }
+        private void DisplayStudent()
+        {
+            Con.Open();
+            string Query = "Select * from StudentTb1";
+            SqlDataAdapter sda = new SqlDataAdapter(Query, Con);
+            SqlCommandBuilder Builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            StudentGV.DataSource = ds.Tables[0];
+            Con.Close();
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        SqlConnection Con = new SqlConnection(@"Data Source=DESKTOP-66B1IDO\SQLEXPRESS;Initial Catalog=SchoolDb;Integrated Security=True");
+
+        private void btnadd_Click(object sender, EventArgs e)
+        {
+            if(StName.Text =="" || FeesTb.Text=="" || AddressTb.Text==""|| StGencb.SelectedIndex == -1 || cbclass.SelectedIndex == -1)
+            {
+                MessageBox.Show("Missing Information");
+            }
+            else
+            {
+                try
+                {
+                    Con.Open();
+                    SqlCommand cmd = new SqlCommand("insert into StudentTb1(StName,StGen,StDOB,StClass,StFees,StAdd) values (@Sname,@SGen,@SDob,@SClass,@SFees,@SAdd)", Con);
+                    cmd.Parameters.AddWithValue("@Sname", StName.Text);
+                    cmd.Parameters.AddWithValue("@SGen", StGencb.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@SDob", DOBPicker.Value.Date);
+                    cmd.Parameters.AddWithValue("@SClass", cbclass.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@SFees", FeesTb.Text);
+                    cmd.Parameters.AddWithValue("@SAdd", AddressTb.Text);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Student Added");
+                    Con.Close();
+                    DisplayStudent();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+               
+
+            }
+        }
+        int key = 0;
+        private void btnclose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void StudentGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void StudentGV_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+            StName.Text = StudentGV.SelectedRows[0].Cells[1].Value.ToString();
+            StGencb.SelectedItem = StudentGV.SelectedRows[0].Cells[2].Value.ToString();
+            DOBPicker.Text = StudentGV.SelectedRows[0].Cells[3].Value.ToString();
+            cbclass.SelectedItem = StudentGV.SelectedRows[0].Cells[5].Value.ToString();
+            FeesTb.Text = StudentGV.SelectedRows[0].Cells[5].Value.ToString();
+            AddressTb.Text = StudentGV.SelectedRows[0].Cells[6].Value.ToString();
+            if (StName.Text == "")
+            {
+                key = 0;
+
+            }
+            else
+            {
+                key = Convert.ToInt32(StudentGV.SelectedRows[0].Cells[0].Value.ToString());
+            }
+        }
+    }
+}
